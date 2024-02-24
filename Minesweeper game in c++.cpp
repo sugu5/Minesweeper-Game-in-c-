88841821344cad6b2152a1c2fcd0bf87753dd678
bsugunathreddy <bsugunathreddy@deloitte.com>
2024-02-24 14:52:23 +0530

    added-more-comments

--- Minesweeper game in c++.cpp
+++ Minesweeper game in c++.cpp
@@ -1,30 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 #define easy 0
 #define medium 1
 #define hard 2
 #define maxside 25
 #define maxmines 99
 #define maxmoves 526 //25*25-99
+
 int side;
 int mine;
+
+// Function to initialize the game boards
 void intialise(char realboard[][maxside], char myboard[][maxside])
 {
-    srand(time (NULL));
+    srand(time (NULL)); // Seed for random number generation
     for (int i=0; i<side; i++)
     {
         for (int j=0; j<side; j++)
-        {            myboard[i][j] = realboard[i][j] = '-';
+        {
+            myboard[i][j] = realboard[i][j] = '-'; // Initialize both boards with empty cells
         }
     }
     return;
 }
+
+// Function to place mines randomly on the real board
 void placemines(int mines[][2],char realboard[][maxside])
 {
     bool mark[maxside*maxside];
-    memset (mark, false, sizeof (mark));
+    memset (mark, false, sizeof (mark)); // Mark array to keep track of placed mines
     for (int i=0; i<mine; )
-     {
+    {
         int random = rand() % (side*side);
         int x = random / side;
         int y = random % side;
@@ -32,13 +39,15 @@ void placemines(int mines[][2],char realboard[][maxside])
         {
             mines[i][0]= x;
             mines[i][1] = y;
-            realboard[mines[i][0]][mines[i][1]] = '*';
-            mark[random] = true;
+            realboard[mines[i][0]][mines[i][1]] = '*'; // Place a mine at (x, y) on the real board
+            mark[random] = true; // Mark the cell as containing a mine
             i++;
         }
     }
     return;
 }
+
+// Function to print the game board
 void printboard(char myboard[][maxside])
 {
     int i, j;
@@ -50,11 +59,13 @@ void printboard(char myboard[][maxside])
     {
         printf ("%d   ", i);
         for (j=0; j<side;j++)
-            printf ("%c ",myboard[i][j]);
+            printf ("%c ",myboard[i][j]); // Print each cell of the board
         printf ("\n");
     }
     return ;
 }
+
+// Function to get user move
 void makemove(int &x,int &y)
 {
     while(true)
@@ -73,6 +84,8 @@ void makemove(int &x,int &y)
     }
     return ;
 }
+
+// Function to check if a cell contains a mine
 bool ismine(int x,int y,char realboard[][maxside])
 {
     if(realboard[x][y]=='*')
@@ -81,72 +94,50 @@ bool ismine(int x,int y,char realboard[][maxside])
     }
     return false;
 }
+
+// Function to replace the first mine if it's in the first move
 void replacemine(int x,int y,char realboard[][maxside])
 {
     for (int i=0; i<side; i++)
     {
         for (int j=0; j<side; j++)
+        {
+            if (realboard[i][j] != '*')
             {
-                if (realboard[i][j] != '*')
-                {
-                    realboard[i][j] = '*';
-                    realboard[x][y] = '-';
-                    return;
-                }
+                realboard[i][j] = '*';
+                realboard[x][y] = '-';
+                return;
             }
+        }
     }
     return;
 }
+
+// Function to check if a cell is within the board bounds
 bool isValid(int row,int col)
 {
     return (row >= 0) && (row < side) && (col >= 0) && (col < side);
 }
+
+// Function to count adjacent mines to a given cell
 int countadjacentmines(int row,int col,char realboard[][maxside])
 {
-    int i=0;
     int count=0;
-        if(isValid (row-1, col) == true)
+    int dx[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dy[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+    for(int i = 0; i < 8; i++)
+    {
+        int nx = row + dx[i];
+        int ny = col + dy[i];
+        if(isValid(nx, ny) && realboard[nx][ny] == '*')
         {
-               if (ismine(row-1, col, realboard)==true)
-               count++;
+            count++;
         }
-        if(isValid(row+1,col)==true)
-        {
-               if (ismine(row+1,col,realboard)==true)
-               count++;
-        }
-        if(isValid (row, col+1) == true)
-        {
-            if (ismine (row, col+1, realboard) == true)
-               count++;
-        }
-        if (isValid (row, col-1) == true)
-        {
-               if (ismine (row, col-1, realboard) == true)
-               count++;
-        }
-        if (isValid (row-1, col+1) == true)
-        {
-            if (ismine (row-1, col+1, realboard) == true)
-               count++;
-        }
-        if (isValid (row-1, col-1) == true)
-        {
-             if (ismine (row-1, col-1, realboard) == true)
-               count++;
-        }
-        if (isValid (row+1, col+1) == true)
-        {
-               if (ismine (row+1, col+1, realboard) == true)
-               count++;
-        }
-        if (isValid (row+1, col-1) == true)
-        {
-            if (ismine (row+1, col-1, realboard) == true)
-               count++;
-        }
-    return (count);
+    }
+    return count;
 }
+
+// Function to play the Minesweeper game
 bool playminesweeper(char myboard[][maxside], char realboard[][maxside],int mines[][2], int row, int col, int *moveleft)
 {
     if(myboard[row][col]!='-')
@@ -171,81 +162,28 @@ bool playminesweeper(char myboard[][maxside], char realboard[][maxside],int mine
         myboard[row][col]=count+'0';
         if(!count)
         {
-            if (isValid (row-1, col) == true)
+            for(int i = -1; i <= 1; i++)
             {
-                   if (ismine (row-1,col,realboard)==false)
-                   playminesweeper(myboard, realboard, mines, row-1, col, moveleft);
-            }
-            if (isValid (row+1, col) == true)
-            {
-                   if (ismine(row+1, col, realboard) == false)
-                    playminesweeper(myboard, realboard, mines, row+1, col, moveleft);
-            }
-            if (isValid (row, col+1) == true)
-            {
-                if (ismine (row, col+1, realboard) == false)
-                    playminesweeper(myboard, realboard, mines, row, col+1, moveleft);
-            }
-            if (isValid (row, col-1) == true)
-            {
-                   if (ismine (row, col-1, realboard) == false)
-                    playminesweeper(myboard, realboard, mines, row, col-1, moveleft);
-            }
-            if (isValid (row-1, col+1) == true)
-            {
-                if (ismine (row-1, col+1, realboard) == false)
-                    playminesweeper(myboard, realboard, mines, row-1, col+1, moveleft);
-            }
-            if (isValid (row-1, col-1) == true)
-            {
-                 if (ismine (row-1, col-1, realboard) == false)
-                    playminesweeper(myboard, realboard, mines, row-1, col-1, moveleft);
-            }
-            if (isValid (row+1, col+1) == true)
-            {
-                 if (ismine (row+1, col+1, realboard) == false)
-                    playminesweeper(myboard, realboard, mines, row+1, col+1, moveleft);
-            }
-            if (isValid (row+1, col-1) == true)
-            {
-                if (ismine (row+1, col-1, realboard) == false)
-                    playminesweeper(myboard, realboard, mines, row+1, col-1, moveleft);
+                for(int j = -1; j <= 1; j++)
+                {
+                    if(i == 0 && j == 0)
+                    {
+                        continue;
+                    }
+                    int nx = row + i;
+                    int ny = col + j;
+                    if(isValid(nx, ny) && myboard[nx][ny] == '-')
+                    {
+                        playminesweeper(myboard, realboard, mines, nx, ny, moveleft);
+                    }
+                }
             }
         }
     }
     return false;
 }
-void playminesweeper()
-{
-    bool gameover=false;
-    char realboard[maxside][maxside],myboard[maxside][maxside];
-    int moveleft=side*side-mine,x,y;
-    int mines[maxmines][2];
-    intialise(realboard,myboard);
-    placemines(mines,realboard);
-    int curmoveindex=0;
-    while(gameover==false)
-    {
-        cout<<"current status of board"<<endl;
-        printboard(myboard);
-        makemove(x,y);
-        if(curmoveindex==0)
-        {
-            if(ismine(x,y,realboard)==true)
-            {
-                replacemine(x,y,realboard);
-            }
-        }
-        curmoveindex++;
-        gameover=playminesweeper(myboard,realboard,mines,x,y,&moveleft);
-        if(gameover==false && moveleft==0)
-        {
-            cout<<"you won "<<endl;
-            gameover=true;
-        }
-    }
-    return ;
-}
+
+// Function to allow the user to choose the difficulty level
 void choosedifficultylevel()
 {
     cout<<"select the level of Game:"<<endl;
@@ -254,36 +192,38 @@ void choosedifficultylevel()
     cout<<"2:medium level"<<endl;
     while(true)
     {
-    int level;
-    cin>>level;
-    if(level==easy)
-    {
-        side=9;
-        mine=10;
-        break;
-    }
-    else if(level==medium)
-    {
-        side=16;
-        mine=40;
-        break;
-    }
-    else if(level==hard)
-    {
-        side=24;
-        mine=99;
-        break;
-    }
-    else
-    {
-        cout<<"your selected invalid level"<<endl;
-        cout<<"please select valid option:"<<endl;
-    }
+        int level;
+        cin>>level;
+        if(level==easy)
+        {
+            side=9;
+            mine=10;
+            break;
+        }
+        else if(level==medium)
+        {
+            side=16;
+            mine=40;
+            break;
+        }
+        else if(level==hard)
+        {
+            side=24;
+            mine=99;
+            break;
+        }
+        else
+        {
+            cout<<"your selected invalid level"<<endl;
+            cout<<"please select valid option:"<<endl;
+        }
     }
 }
+
+// Main function
 int main()
 {
-    choosedifficultylevel();
-    playminesweeper();
+    choosedifficultylevel(); // Choose the difficulty level
+    playminesweeper(); // Start the game
     return 0;
 }
